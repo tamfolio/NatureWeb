7746bea194a545102fd3d6255b27df22c915680f
unknown <tamiloreolaniyi@gmail.com>
2022-04-10 21:28:43 +0100

    Header Text

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,15 @@ import './Header.css'
 function Header() {
   return (
     <header className='header'>
+        <div className="logo-box">
+          <img src="/images/logo-white.png" alt="Logo" className='logo'/>
+        </div>
+    <div className="text-box">
+    <h1 className="primary-header">
+          <span className="main-primary">Outdoors</span>
+          <span className="sub-primary">Is where Life Happens</span>
+        </h1>
+    </div>
         
     </header>
   )
